871eac8d5983fcd1782b2d8e3f72f9a0a16ced5c
Kaustubh <ksd.tcet@gmail.com>
2024-10-09 23:41:58 +0530

    add 2 files

---
+++ Episode 3/BLPattern.kt
@@ -0,0 +1,14 @@
+import com.faangx.ktp.patterns.single.PatternLinesAndCharBasedMiniApp
+
+fun printPattern(lines: Int, char: Char) {
+    repeat(lines) {
+        repeat(it + 1) {
+            print(char)
+        }
+        println()
+    }
+}
+
+fun main() {
+    PatternLinesAndCharBasedMiniApp("BL", ::printPattern)
+}
