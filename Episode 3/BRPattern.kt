43e4596e3511685a9ede466de7b7292b29535d2d
Kaustubh <ksd.tcet@gmail.com>
2024-10-11 03:20:28 +0530

    added BR Pattern

---
+++ Episode 3/BRPattern.kt
@@ -0,0 +1,13 @@
+import com.faangx.ktp.patterns.single.PatternLinesAndCharBasedMiniApp
+
+fun printPattern(lines: Int, char: Char) {
+    repeat(lines){
+        repeat(lines - 1 - it){ print(' ')}
+        repeat(it + 1 ){ print(char)}
+        println()
+    }
+}
+
+fun main() {
+    PatternLinesAndCharBasedMiniApp("BR", ::printPattern)
+}
