9f01ee5a5a2eccc53b4a04c7821c1f25b909030f
Kaustubh <ksd.tcet@gmail.com>
2024-10-11 03:21:40 +0530

    added TLN pattern

---
+++ Episode 3/TLNPattern.kt
@@ -0,0 +1,12 @@
+import com.faangx.ktp.patterns.single.PatternLinesBasedMiniApp
+
+fun printPattern(lines: Int) {
+    repeat(lines){ i ->
+        repeat(lines - i){ print(lines - i) }
+        println()
+    }
+}
+
+fun main() {
+    PatternLinesBasedMiniApp("TLN", ::printPattern)
+}
