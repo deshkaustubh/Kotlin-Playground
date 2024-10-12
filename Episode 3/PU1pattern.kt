1dddc1d85fea00df7070c1eb4108641f08f40630
Kaustubh <ksd.tcet@gmail.com>
2024-10-12 15:49:23 +0530

    pattern commit

---
+++ Episode 3/PU1pattern.kt
@@ -0,0 +1,15 @@
+package `Episode 3`
+
+import com.faangx.ktp.patterns.single.PatternLinesAndCharBasedMiniApp
+
+fun printPattern(lines: Int, char: Char) {
+    repeat(lines){i->
+        repeat(lines - i - 1){ print(" ")}
+        repeat(2 * i + 1){ print(char)}
+        println()
+    }
+}
+
+fun main() {
+    PatternLinesAndCharBasedMiniApp("PU1", ::printPattern)
+}
