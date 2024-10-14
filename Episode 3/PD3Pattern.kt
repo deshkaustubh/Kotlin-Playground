073ca8afc9b72dadcd77c570598700871e52a46f
Kaustubh <ksd.tcet@gmail.com>
2024-10-14 22:30:54 +0530

    Added PD3 Pattern

---
+++ Episode 3/PD3Pattern.kt
@@ -0,0 +1,21 @@
+package `Episode 3`
+
+import com.faangx.ktp.patterns.single.PatternLinesAndTwoCharsBasedMiniApp
+
+fun printPattern(lines: Int, char1: Char, char2: Char) {
+    repeat(lines){i ->
+        // Space
+        repeat(i){print(" ")}
+
+        repeat(2 * (lines - i) - 1) { j ->
+            print(
+                if (j % 2 == 0) (char1) else char2
+            )
+        }
+        println()
+    }
+}
+
+fun main() {
+    PatternLinesAndTwoCharsBasedMiniApp("PD3", ::printPattern)
+}
