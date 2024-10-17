542307c6fd7da961ee5689a73a71fbaf77bd5a85
Kaustubh <ksd.tcet@gmail.com>
2024-10-17 19:40:52 +0530

    added pattern 3

---
+++ Episode3Extended/pattern3.kt
@@ -0,0 +1,17 @@
+package Episode3Extended
+
+import com.faangx.ktp.patterns.single.PatternLinesAndTwoCharsBasedMiniApp
+
+fun printPattern(lines: Int, char1: Char, char2: Char) {
+    repeat(lines) { i ->
+        repeat(2 * lines - 1) { j ->
+            print(
+                if (j == lines - 1 - i || j == lines - 1 + i) char1 else char2
+            )
+        }
+        println()
+    }
+}
+fun main() {
+    PatternLinesAndTwoCharsBasedMiniApp("P3", ::printPattern)
+}
