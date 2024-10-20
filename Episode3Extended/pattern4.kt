49e137fd873e75a4628c58f1a693ad6881e8f711
Kaustubh <ksd.tcet@gmail.com>
2024-10-20 18:53:04 +0530

    pattern 4 updated

---
+++ Episode3Extended/pattern4.kt
@@ -0,0 +1,18 @@
+package Episode3Extended
+
+import com.faangx.ktp.patterns.single.PatternLinesAndTwoCharsBasedMiniApp
+
+fun printPattern(lines: Int, char1: Char, char2: Char) {
+    repeat(lines) { i ->
+        repeat(i + 1) { print(char1) }
+        repeat(lines) { print(char2) }
+        repeat((lines - i) * 2) { print(char1) }
+        repeat(lines) { print(char2) }
+        repeat(i + 1) { print(char1) }
+        println()
+    }
+}
+
+fun main() {
+    PatternLinesAndTwoCharsBasedMiniApp("P4", ::printPattern)
+}
