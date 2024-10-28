2aabcb497fac6dc971d0485f9be21ebec1b81f2d
Kaustubh <ksd.tcet@gmail.com>
2024-10-28 20:11:18 +0530

    completed assignment 3 part 2

---
+++ EP4AssignmentPrograms/src/main/kotlin/EP4Assignment3/EP4Assignment3part2.kt
@@ -0,0 +1,22 @@
+package EP4Assignment3
+
+/*
+3.2 Harshad Numbers
+ Check if a number is a Harshad number, which is an integer that is divisible by the sum of its
+digits. For example, 18 is a Harshad number because (1 + 8 = 9) and (18 / 9 = 2).
+ */
+
+fun main(){
+    println(checkHarshad(18))
+}
+
+fun checkHarshad(num: Int): Boolean{ // Here interestingly I took help of print reverse number program
+    var sum = 0
+    var number = num
+    while(number != 0){
+        val digit = number % 10
+        sum = sum + digit
+        number = number / 10
+    }
+    if (num % sum != 0) return false else return true
+}
