daf855a96f3f09349422985d05b2160fa58cd5ea
Kaustubh <ksd.tcet@gmail.com>
2024-10-28 19:49:02 +0530

    completed assignment 3 part 1

---
+++ EP4AssignmentPrograms/src/main/kotlin/EP4Assignment3/EP4Assignment3part1.kt
@@ -0,0 +1,21 @@
+package EP4Assignment3
+
+/*
+3.1 Perfect Numbers
+ A positive integer that is equal to the sum of its proper divisors (excluding itself). For example,
+28 is a perfect number because (1 + 2 + 4 + 7 + 14 = 28).
+ */
+
+fun main(){
+    println(checkPerfect(28))
+}
+
+fun checkPerfect(number: Int): Boolean{
+    var sum : Int = 0
+    for(i in 1 until number){ // Did the logic on my own but in place of until i used .. so it wasn't working at this point i used copilot's help
+        if(number % i == 0){
+            sum = sum + i
+        }
+    }
+    if (sum == number) return true else return false
+}
