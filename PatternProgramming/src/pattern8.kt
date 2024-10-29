46d2a74752cc57c616c30d2745fe31ce3444cd03
Kaustubh <ksd.tcet@gmail.com>
2024-10-29 14:35:35 +0530

    pattern 8 added

--- PatternProgramming/src/pattern8.kt
+++ PatternProgramming/src/pattern8.kt
@@ -1,5 +1,10 @@
 /*
-
+Q 25 of pdf
+                1
+             2  3  4
+          5  6  7  8  9
+      10 11 12 13 14 15 16
+   17 18 19 20 21 22 23 24 25
  */
 
 fun main() {
@@ -9,10 +14,16 @@ fun main() {
 fun pattern8(lines: Int) {
     repeat(lines){
         repeat(lines - it){
-            print(" ")
+            print("   ") // 3 spaces
         }
         repeat(2 * it + 1 ){j->
-            print(it + j + 1)
+            var digit = it * it + 1 + j
+            if(digit / 10 == 0){
+                print(" $digit ") // 1 space in back and front each
+            }
+            else{
+                print("$digit ") // 1 space in end because it is 2 digit
+            }
         }
         println()
     }
