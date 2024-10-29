7a10fbe4473cd0fc37081abfecdbb9f34926914b
Kaustubh <ksd.tcet@gmail.com>
2024-10-29 10:41:25 +0530

    program updated without using math.pow

--- EP4AssignmentPrograms/src/main/kotlin/EP4Assignment3/EP4Assignment3part3.kt
+++ EP4AssignmentPrograms/src/main/kotlin/EP4Assignment3/EP4Assignment3part3.kt
@@ -7,21 +7,20 @@ same digits as the number itself. For example, 25 is an automorphic number becau
 625).
  */
 
+
 fun main(){
     printAutomorphicNumbersUpTo(100)
 }
 
 fun checkAutomorphic(num: Int): Boolean { // Here interestingly I took help of print reverse number program
+    var powerDigit = digitChecker(num)
     var squareofNumber = num * num
-    var digit = 0
-        var number = num
-        while (number != 0) {
-            number = number / 10
-            digit = digit + 1
-        }
-
-    var resultVariable = (squareofNumber % (Math.pow(10.0, digit.toDouble()))).toInt()
-
+    var divisor = 1
+    while (powerDigit != 0){
+        powerDigit = powerDigit -1
+        divisor = divisor * 10
+    }
+    val resultVariable = squareofNumber % divisor
     return resultVariable == num
 }
 
@@ -32,3 +31,40 @@ fun printAutomorphicNumbersUpTo(upTo: Int) {
         }
     }
 }
+
+fun digitChecker(num: Int): Int {
+    var digit = 0
+    var number = num
+    while (number != 0) {
+        number = number / 10
+        digit = digit + 1
+    }
+    return digit
+}
+
+
+//fun main(){
+//    printAutomorphicNumbersUpTo(100)
+//}
+//
+//fun checkAutomorphic(num: Int): Boolean { // Here interestingly I took help of print reverse number program
+//    var squareofNumber = num * num
+//    var digit = 0
+//        var number = num
+//        while (number != 0) {
+//            number = number / 10
+//            digit = digit + 1
+//        }
+//
+//    var resultVariable = (squareofNumber % (Math.pow(10.0, digit.toDouble()))).toInt()
+//
+//    return resultVariable == num
+//}
+//
+//fun printAutomorphicNumbersUpTo(upTo: Int) {
+//    for (i in 1..upTo) {
+//        if (checkAutomorphic(i)) {
+//            println("$i -> Automorphic number")
+//        }
+//    }
+//}
--- EP4AssignmentPrograms/src/main/kotlin/Main.kt
+++ EP4AssignmentPrograms/src/main/kotlin/Main.kt
@@ -1,1 +1,35 @@
 package org.example
+
+fun main(){
+    printAutomorphicNumbersUpTo(100)
+}
+
+fun checkAutomorphic(num: Int): Boolean { // Here interestingly I took help of print reverse number program
+    var powerDigit = digitChecker(num)
+    var squareofNumber = num * num
+    var divisor = 1
+    while (powerDigit != 0){
+        powerDigit = powerDigit -1
+        divisor = divisor * 10
+    }
+    val resultVariable = squareofNumber % divisor
+    return resultVariable == num
+}
+
+fun printAutomorphicNumbersUpTo(upTo: Int) {
+    for (i in 1..upTo) {
+        if (checkAutomorphic(i)) {
+            println("$i -> Automorphic number")
+        }
+    }
+}
+
+fun digitChecker(num: Int): Int {
+    var digit = 0
+    var number = num
+    while (number != 0) {
+        number = number / 10
+        digit = digit + 1
+    }
+    return digit
+}
