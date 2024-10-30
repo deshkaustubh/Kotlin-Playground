c2605e6c50488c940840d55d4658200bde003873
Kaustubh <ksd.tcet@gmail.com>
2024-10-30 10:41:38 +0530

    added minor imporvemetns

--- EP4AssignmentPrograms/src/main/kotlin/EP4Assignment3/EP4Assignment3part3.kt
+++ EP4AssignmentPrograms/src/main/kotlin/EP4Assignment3/EP4Assignment3part3.kt
@@ -13,12 +13,12 @@ fun main(){
 }
 
 fun checkAutomorphic(num: Int): Boolean { // Here interestingly I took help of print reverse number program
-    var powerDigit = digitChecker(num)
-    var squareofNumber = num * num
+    var noOfDigits = noOfDigits(num)
+    val squareofNumber = num * num
     var divisor = 1
-    while (powerDigit != 0){
-        powerDigit = powerDigit -1
-        divisor = divisor * 10
+    while (noOfDigits != 0){
+        noOfDigits -= 1
+        divisor *= 10
     }
     val resultVariable = squareofNumber % divisor
     return resultVariable == num
@@ -32,12 +32,12 @@ fun printAutomorphicNumbersUpTo(upTo: Int) {
     }
 }
 
-fun digitChecker(num: Int): Int {
+fun noOfDigits(num: Int): Int {
     var digit = 0
     var number = num
     while (number != 0) {
-        number = number / 10
-        digit = digit + 1
+        number /= 10
+        digit += 1
     }
     return digit
 }
